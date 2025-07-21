bccb223229753cf8b0b1e91d5fc7cdfca317b12c
witnes sostenes <112706208+nshobairwe@users.noreply.github.com>
2025-07-21 15:47:37 +0300

    website v4

--- src/components/TheLastComponent.vue
+++ src/components/TheLastComponent.vue
@@ -8,15 +8,28 @@ import Logo from "../assets/logo.jpg";
     <div class="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6">
       <!-- Contact Info -->
       <div class="space-y-3 text-sm md:text-base">
-        <div class="flex items-center gap-3 px-9">
-          
-          CONTACT US<br>
-          Open Sea Company Ltd<br>
-        88 Ghuba Road, House No 120, Plot<br>
-            No 227, Msasani Village<br>
-         +255 747 100 444<br>
-         Dar es salaam
-        </div>
+        <div class="flex flex-col gap-2 px-9 text-sm md:text-base">
+            <div class="flex items-center gap-2">
+<h3>CONTACT US</h3>
+            </div>
+    
+  <div class="flex items-center gap-2">
+    <i class="pi pi-phone text-lg"></i>
+    <span>+255 747 100 444</span>
+  </div>
+  <div class="flex items-center gap-2">
+    <i class="pi pi-building text-lg"></i>
+    <span>Open Sea Company Ltd</span>
+  </div>
+  <div class="flex items-center gap-2">
+    <i class="pi pi-map-marker text-lg"></i>
+    <span>88 Ghuba Road, House No 120, Plot No 227, Msasani Village</span>
+  </div>
+  <div class="flex items-center gap-2">
+    <i class="pi pi-map text-lg"></i>
+    <span>Dar es Salaam</span>
+  </div>
+</div>
         
       </div>
 
